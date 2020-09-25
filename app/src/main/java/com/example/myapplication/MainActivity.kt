66088c512b117f9b9b3004757f049653991fbae3
Thiago Smanioto <thiagosmanioto.eng@gmail.com>
2020-09-24 21:35:00 -0400

    Questão 7 - Prova

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.myapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mudarParaParteDois()
+    }
+
+    private fun mudarParaParteDois() {
+        val intent = Intent(this, ParteDois::class.java)
+
+        Handler().postDelayed({
+            intent.change()
+        }, 2000)
+    }
+
+    fun Intent.change() {
+        startActivity(this)
+        finish()
+    }
+
+}
